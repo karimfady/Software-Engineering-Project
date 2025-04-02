eaf0b3f51884f1165628cceacb527bec121fe9fd
Hoid858 <ziadg@aucegypt.edu>
2025-04-02 20:01:01 +0200

    project files

--- outfiltyapplication/android/app/src/main/kotlin/com/example/outfiltyapplication/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.outfiltyapplication
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
---
+++ outfitlyapp/android/app/src/main/kotlin/com/example/outfitlyapp/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.outfitlyapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
