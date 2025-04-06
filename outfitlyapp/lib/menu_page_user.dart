7ea455bf446845fbcef8aa951dc717fac984d132
mostafans0 <mostafans0@aucegypt.edu>
2025-04-06 15:54:45 +0200

    Fixed a minor issue in the menu_page_user

--- outfitlyapp/lib/menu_page_user.dart
+++ outfitlyapp/lib/menu_page_user.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'login_logic.dart';
-import 'wishlist_state.dart';
 import 'login_page.dart';
 
 class MenuPage extends StatefulWidget {
@@ -13,56 +12,12 @@ class MenuPage extends StatefulWidget {
 
 class _MenuPageState extends State<MenuPage> {
   @override
-  void initState() {
-    super.initState();
-    WishlistState().updateWishlistCount();
-  }
-
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Menu'), centerTitle: true),
       body: ListView(
         padding: const EdgeInsets.all(16.0),
         children: [
-          ListTile(
-            leading: const Icon(Icons.favorite),
-            title: Row(
-              children: [
-                const Text('Wishlist'),
-                const SizedBox(width: 8),
-                ListenableBuilder(
-                  listenable: WishlistState(),
-                  builder: (context, _) {
-                    final count = WishlistState().itemCount;
-                    if (count == 0) return const SizedBox.shrink();
-                    return Container(
-                      padding: const EdgeInsets.symmetric(
-                        horizontal: 8,
-                        vertical: 2,
-                      ),
-                      decoration: BoxDecoration(
-                        color: Colors.red,
-                        borderRadius: BorderRadius.circular(12),
-                      ),
-                      child: Text(
-                        count.toString(),
-                        style: const TextStyle(
-                          color: Colors.white,
-                          fontSize: 12,
-                        ),
-                      ),
-                    );
-                  },
-                ),
-              ],
-            ),
-            onTap: () {
-              Navigator.pushNamed(context, '/wishlist').then((_) {
-                WishlistState().updateWishlistCount();
-              });
-            },
-          ),
           // My Account Section
           const Text(
             'My Account',
